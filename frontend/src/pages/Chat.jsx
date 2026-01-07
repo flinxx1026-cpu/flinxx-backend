b9a3bb73f97183c3a995770ee01544b1a59b5183
Nikhil <flinxx1026@gmail.com>
2026-01-07 22:08:32 +0530

    Fix magnifying glass position - center above heading text

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -291,9 +291,9 @@ const WaitingScreen = React.memo(({ onCancel, localStreamRef, cameraStarted }) =
           display: inline-block;
           width: fit-content;
           height: fit-content;
-          top: 60px;
-          left: 80px;
-          transform: none;
+          top: 20px;
+          left: 50%;
+          transform: translateX(-50%);
         }
         @keyframes searchFloat {
           0% {
