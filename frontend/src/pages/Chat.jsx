dbb3050ec957e5103c959e2652768ef01fed01b4
Nikhil <flinxx1026@gmail.com>
2026-01-07 22:01:17 +0530

    Move magnifying glass to left side of waiting screen

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -291,9 +291,9 @@ const WaitingScreen = React.memo(({ onCancel, localStreamRef, cameraStarted }) =
           display: inline-block;
           width: fit-content;
           height: fit-content;
-          top: -100px;
-          left: 50%;
-          transform: translateX(-50%);
+          top: 60px;
+          left: 80px;
+          transform: none;
         }
         @keyframes searchFloat {
           0% {
